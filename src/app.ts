a8fe0b4dbdedadbd49da6e43c34403e3fbb54a88
Jabes Bueno <jabesbueno@hotmail.com>
2021-10-23 14:09:55 -0300

    Criação e validação de listagem de users criados

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
+
 app.use(router);
 
 app.get("/github", (request, response)=>{
@@ -15,5 +33,7 @@ app.get("/signin/callback", (request, response)=>{
     const {code} = request.query;
     return response.json(code);
     //response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
-app.listen(4000, () => console.log(`:rocket Server is running on Port 4000`));
+});
+
+export { serverHttp, io };
+
